ecbbdb501d31b7f80191e03ed35529252f1291b4
Noah Braunfeld <noah@braunfeld.dev>
2025-09-18 19:42:51 -0600

    added socket scaffolding as well as socketaddr scaffolding, still need to implement async io

--- socket.h
+++ socket.h
@@ -1,16 +1,23 @@
 #ifndef SOCKET_H_
 #define SOCKET_H_
 #include <concepts>
+#include <cstddef>
+#include <cstdint>
 #include <cstring>
+#include <memory>
 #include <netinet/in.h>
 #include <optional>
 
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <span>
+#include <string>
+#include <string_view>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <system_error>
 #include <unistd.h>
+#include <utility>
 
 struct sockaddr;
 struct sockaddr_in;
@@ -45,7 +52,99 @@ private:
   int _fd{-1};
 };
 
-class SocketAddr {};
+class SocketAddr {
+public:
+  static inline const std::string LOCALHOST = "127.0.0.1";
+  SocketAddr() noexcept;
+  SocketAddr(std::string_view ip, uint16_t port);
+
+  [[nodiscard]] const sockaddr *asCType() const noexcept;
+  [[nodiscard]] sockaddr *asCType() noexcept;
+  [[nodiscard]] std::size_t size() const noexcept;
+
+  [[nodiscard]] uint16_t port() const noexcept;
+  [[nodiscard]] std::string ip() const;
+  [[nodiscard]] uint32_t ipValue() const noexcept;
+
+  [[nodiscard]] bool operator==(const SocketAddr &otherAddr) const noexcept;
+  [[nodiscard]] bool
+  operator<=>(const SocketAddr &otherAddr) const noexcept = default;
+
+  [[nodiscard]] std::string toString() const;
+
+private:
+  struct Impl; // the underlying socket from libc
+  std::unique_ptr<Impl> _impl;
+  friend class Socket;
+};
+
+struct SocketOptions {
+  // TODO: figure out which options I need to add;
+};
+
+class Socket {
+public:
+  enum class Type {
+    TCP,
+    UDP, // TODO: add udp support (not needed for this lab)
+  };
+
+  enum class State {
+    // TODO: figure out which states the socket can be in
+  };
+
+  explicit Socket(Type type = Type::TCP);
+  Socket(Socket &&other) noexcept;
+  Socket &operator=(Socket &&other) noexcept;
+  ~Socket() noexcept;
+
+  Socket(const Socket &) = delete;
+  Socket &operator=(const Socket &) = delete;
+
+  [[nodiscard]] static std::optional<Socket> create(Type type = Type::TCP);
+  [[nodiscard]] static std::optional<Socket> createBind(const SocketAddr &addr,
+                                                        Type type = Type::TCP);
+
+  // TODO: add support for configuration with SocketOptions
+
+  [[nodiscard]] bool bind(const SocketAddr &addr);
+  [[nodiscard]] bool listen(int backlog = 128);
+  [[nodiscard]] std::optional<std::pair<Socket, SocketAddr>> accept();
+  [[nodiscard]] bool connect(const SocketAddr &addr);
+
+  [[nodiscard]] std::optional<std::size_t>
+  send(std::span<const std::byte> data);
+  [[nodiscard]] std::optional<std::string>
+  send(std::size_t maxStringSize = 4096);
+  [[nodiscard]] std::optional<std::size_t>
+  sendTo(std::span<const std::byte> data, const SocketAddr &addr);
+
+  [[nodiscard]] std::optional<std::size_t>
+  recv(std::span<const std::byte> buffer);
+  [[nodiscard]] std::optional<std::size_t> recv(std::string_view data);
+  [[nodiscard]] std::optional<std::pair<std::size_t, SocketAddr>>
+  recvFrom(std::span<std::byte> buffer);
+
+  [[nodiscard]] bool isValid() const noexcept;
+  [[nodiscard]] int fd() const noexcept;
+  [[nodiscard]] State state() const noexcept;
+  [[nodiscard]] Type type() const noexcept;
+
+  [[nodiscard]] std::optional<SocketAddr> localAddress() const;
+  [[nodiscard]] std::optional<SocketAddr> remoteAddress() const;
+
+  bool shutdown();
+  void close() noexcept;
+
+  [[nodiscard]] error_code lastError() const noexcept;
+
+private:
+  struct Impl; // socket underlying implementation (so I can work on both my mac
+               // device and ubuntu)
+  std::unique_ptr<Impl> _impl;
+};
+
+class Poll {};
 
 } // namespace wnet
 #endif
