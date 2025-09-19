37622255467d1b144df48014128b1b7c5223c56f
Noah Braunfeld <noah@braunfeld.dev>
2025-09-19 12:58:45 -0600

    begin implementation

---
+++ socket.cpp
@@ -0,0 +1,15 @@
+#include "socket.h"
+#include <arpa/inet.h>
+#include <cerrno>
+#include <cstddef>
+#include <cstring>
+#include <format>
+#include <memory>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <sys/socket.h>
+#include <system_error>
+#include <unistd.h>
+
+namespace wnet {
+} // namespace wnet
